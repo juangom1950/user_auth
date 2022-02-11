feeb7ee02d5e6f5525df4c8e6663c45a2e8e4802
juangom1950 <juangom73@gmail.com>
2022-02-10 23:42:36 -0500

    User Password Authentication

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,9 +1,11 @@
 const usersDB = {
+    // This simulates the Db
     users: require('../model/users.json'),
     setUsers: function (data) { this.users = data }
 }
 const fsPromises = require('fs').promises;
 const path = require('path');
+//npm i bcrypt
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
@@ -14,10 +16,12 @@ const handleNewUser = async (req, res) => {
     if (duplicate) return res.sendStatus(409); //Conflict 
     try {
         //encrypt the password
+        // 10 is the salt that we are adding
         const hashedPwd = await bcrypt.hash(pwd, 10);
         //store the new user
         const newUser = { "username": user, "password": hashedPwd };
         usersDB.setUsers([...usersDB.users, newUser]);
+        // This will overwrite any existing file there
         await fsPromises.writeFile(
             path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users)
