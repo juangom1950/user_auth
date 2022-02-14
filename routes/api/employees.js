f8098b0c2e5f8481177c8c001ae44ecb8079d2aa
juangom1950 <juangom73@gmail.com>
2022-02-14 16:58:51 -0500

    Add the verifyRoles middleware to routes

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
 const verifyJWT = require('../../middleware/verifyJWT');
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/')
-    .get(verifyJWT, employeesController.getAllEmployees)
-    .post(verifyJWT, employeesController.createNewEmployee)
-    .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee);
+    .get(employeesController.getAllEmployees)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
 router.route('/:id')
     .get(employeesController.getEmployee);
